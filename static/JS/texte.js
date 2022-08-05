406b38fc2f93fbf407b07f354e69a751aaa7d25a
Fabrício Jaime <55202973+fabriciojaime@users.noreply.github.com>
2022-08-05 10:18:09 -0300

    remove test archive

--- static/JS/texte.js
+++
@@ -1,8 +0,0 @@
-// let elem = document.getElementById('type-here');
-
-document.addEventListener("keydown", function (event) {
-    console.log(event.keyCode);
-  	if(event.keyCode == 82){
-        alert('TECLA R')
-    }
-});
